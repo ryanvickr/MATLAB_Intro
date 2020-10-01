ecdcc48517a1564b74e8414e08b92bfc37c0a351
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-10-01 17:09:01 -0400

    Added information on indexing

--- MATLAB_Intro.m
+++ MATLAB_Intro.m
@@ -23,3 +23,20 @@ n = 3;
 ones_matrix = ones(n)
 % create a 'n'x'n' matrix of zeros
 zeros_matrix = zeros(n)
+
+
+%-----Indexing Vectors-----
+% get the 2nd value from a vector
+row_vector(2) % NOTE: MATLAB starts indexes from 1, and NOT 0!!!
+% get the last element from a vector
+row_vector(end)
+% get the length of a vector
+length(row_vector)
+
+%-----Indexing Matrices-----
+% get the 0,1 element (first row, second column) of a matrix
+matrix(1,2) % REMEMBER, MATLAB starts indexes from 1, NOT 0!!!
+% get the entire 2nd row of a matrix
+matrix(2,:)
+% get rows 2->3 of a matrix
+matrix(2:3,:)
