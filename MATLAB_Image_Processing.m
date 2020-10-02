004d5faec2bca8c3f0878a8c4aa7a7e223eb4152
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-10-02 17:45:30 -0400

    Created image processing script.

---
+++ MATLAB_Image_Processing.m
@@ -0,0 +1,7 @@
+% MATLAB Image Processing
+
+% we can load in an image using imread:
+im = imread('pout.tif'); % we use single quotes for strings
+
+% we can get info on the file without loading it into MATLAB
+imfinfo('pout.tif')
